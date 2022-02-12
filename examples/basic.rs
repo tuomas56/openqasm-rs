8f319420f823cdf21ef0b1638573787ecb0985ca
Tuomas Laakkonen <pigworts2@gmail.com>
2022-02-12 00:42:45 +0000

    Add an example

---
+++ examples/basic.rs
@@ -0,0 +1,24 @@
+use openqasm as oq;
+use oq::GenericError;
+
+fn example(path: &str, cache: &mut oq::SourceCache) -> Result<(), oq::Errors> {
+    let mut parser = oq::Parser::new(cache);
+    parser.parse_file(path);
+    let program = parser.done().to_errors()?;
+    program.type_check().to_errors()?;
+    Ok(())
+}
+
+fn main() {
+    let mut cache = oq::SourceCache::new();
+
+    println!("Processing: good.qasm:");
+    if let Err(errors) = example("examples/good.qasm", &mut cache) {
+        errors.print(&mut cache).unwrap();
+    }
+
+    println!("Processing: bad.qasm:");
+    if let Err(errors) = example("examples/bad.qasm", &mut cache) {
+        errors.print(&mut cache).unwrap();
+    }
+}
